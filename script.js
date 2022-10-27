7f15dbd10f0b13df05c88ecf435c9533cbb1f16b
franckwilfried <wilfiedfranck@gmail.com>
2022-10-27 19:55:33 +0200

    app_02

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild(renderer.domElement);
+// function render() {
+// requestAnimationFrame(render);
+// renderer.render(scene, camera);
+// }
+// render();
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z=5;
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
+var sphGeo= new THREE.SphereGeometry(1,32,32);
+var sphMat=new THREE.MeshBasicMaterial({color: 0xff0000});
+var sph = new THREE.Mesh(sphGeo,sphMat);
+scene.add(sph);
 function render() {
 requestAnimationFrame(render);
 renderer.render(scene, camera);
