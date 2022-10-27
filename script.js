565b3f2601c6a88917f8184006b3a7f82caf6fe2
franckwilfried <wilfiedfranck@gmail.com>
2022-10-27 23:17:27 +0200

    app_05

--- script.js
+++ script.js
@@ -35,8 +35,19 @@ camera.lookAt(scene.position); //Exécutin du positionnement avant la rotation c
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
+
+var boxGeo= new THREE.BoxGeometry(1,1,1);
+var boxMat=new THREE.MeshBasicMaterial({color: 0x6c83f4});
+var box = new THREE.Mesh(boxGeo,boxMat);
+
+var sphGeo= new THREE.SphereGeometry(1,32,32);
+var sphMat=new THREE.MeshBasicMaterial({color: 0xff0000});
+var sph = new THREE.Mesh(sphGeo,sphMat);
+sph.position.set(0 , 2 , 0);
+
 var axisHelper = new THREE.AxesHelper(5);
-scene.add(axisHelper);
+scene.add(axisHelper , box , sph);
+
 function render() {
 requestAnimationFrame(render);
 renderer.render(scene, camera);
