d5f6c20b8fbbcd8cb3de1bbada8281464eb890e2
franckwilfried <wilfiedfranck@gmail.com>
2022-10-28 17:50:00 +0200

    app_09

--- script.js
+++ script.js
@@ -1,56 +1,33 @@
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild(renderer.domElement);
-// function render() {
-// requestAnimationFrame(render);
-// renderer.render(scene, camera);
-// }
-// render();
-
-
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-// camera.position.z=5;
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth,window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-// var sphGeo= new THREE.BoxGeometry(1,1,1);
-// var sphMat=new THREE.MeshBasicMaterial({color: 0x6c83f4});
-// var sph = new THREE.Mesh(sphGeo,sphMat);
-// scene.add(sph);
-// function render() {
-// requestAnimationFrame(render);
-// renderer.render(scene, camera);
-// }
-// render();
+var vAngX=0.02;
+var vAngY=0.03;
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000);
-camera.position.set(5 , 5 , 5);
-camera.lookAt(scene.position); //Exécutin du positionnement avant la rotation camera
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.set(0,0,300) ; 
+camera.lookAt(scene.position);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
-// var lightAmb = new THREE.AmbientLight (0xffffff) ;
-var lightSpot = new THREE.SpotLight (0xffffff, 1, 100, Math.PI/4) ;
-var lightpoi = new THREE.PointLight(0xffffff , 1 , 1000)
-lightpoi.position.set(0 , 5 , 0)
-lightSpot.position.set(0 , 1 , 5)
+var axisHelper = new THREE.AxesHelper(70);
+scene.add(axisHelper);
 
+var boxGeo= new THREE.BoxGeometry(50,50,50);
+var boxMat=new THREE.MeshLambertMaterial({color: 0xff00ff});
+var box = new THREE.Mesh(boxGeo,boxMat);
+scene.add(box);
 
-const geometry = new THREE.SphereGeometry( 2 , 32 , 32 );
-const material = new THREE.MeshPhongMaterial( { color: 0xffff00 , flatShading : true} );
-const sphere = new THREE.Mesh( geometry, material );
-
-lightSpot.target = sphere
-var axisHelper = new THREE.AxesHelper(5);
-scene.add(axisHelper , sphere , lightpoi , lightSpot);
+var pointLight = new THREE.PointLight( 0xffffff);
+pointLight.position.set(60,60,60);
+scene.add(pointLight);
+var spotLight = new THREE.SpotLight( 0xffffff);
+spotLight.position.set(-300,0,0);
+scene.add(spotLight);
 
 function render() {
 requestAnimationFrame(render);
+box.rotation.y=box.rotation.y+vAngY
+box.rotation.x=box.rotation.x+vAngX
 renderer.render(scene, camera);
 }
 render();
