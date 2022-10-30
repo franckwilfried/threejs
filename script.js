3d4ce6b863ea9aa27dd0e611c75f6caf8e5ec922
franckwilfried <wilfiedfranck@gmail.com>
2022-10-30 14:48:12 +0100

    app_10

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var vAngX=0.02;
-var vAngY=0.03;
+var chrono=new THREE.Clock();
+chrono.start() ;
+var temps=chrono.getElapsedTime();
+var vAng=1;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.set(0,0,300) ; 
+camera.position.set(0,0,300)
 camera.lookAt(scene.position);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,14 +13,14 @@ document.body.appendChild(renderer.domElement);
 
 var axisHelper = new THREE.AxesHelper(70);
 scene.add(axisHelper);
-
 var boxGeo= new THREE.BoxGeometry(50,50,50);
 var boxMat=new THREE.MeshLambertMaterial({color: 0xff00ff});
 var box = new THREE.Mesh(boxGeo,boxMat);
+
 scene.add(box);
 
 var pointLight = new THREE.PointLight( 0xffffff);
-pointLight.position.set(60,60,60);
+pointLight.position.set( 60, 60, 60 );
 scene.add(pointLight);
 var spotLight = new THREE.SpotLight( 0xffffff);
 spotLight.position.set(-300,0,0);
@@ -26,8 +28,9 @@ scene.add(spotLight);
 
 function render() {
 requestAnimationFrame(render);
-box.rotation.y=box.rotation.y+vAngY
-box.rotation.x=box.rotation.x+vAngX
+temps=temps+1/30
+box.rotation.y=vAng*temps;
 renderer.render(scene, camera);
 }
+
 render();
